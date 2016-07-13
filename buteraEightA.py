a7f653d127b92594aa9a9c532dd3b95dacc7fb7c
Andrew Kennedy <amk319@scarletmail.rutgers.edu>
2016-07-13 12:13:58 -0400

    Converted units

--- buteraEightA.py
+++ buteraEightA.py
@@ -16,20 +16,20 @@ class Rhs(object):
         s = self
         s.N = N    # Note that all units are in terms of milli (10**-3)
         s.A = np.zeros((N , N))
-        s.gNa = 0.000028    # 28nS
+        s.gNa = 2.8    # nS
         s.VNa = 50    # 50mV
-        s.gk = 0.0000112    # 11.2nS
+        s.gk = 11.2    # 11.2nS
         s.Vk = -85.0   # -85mV
-        s.gNap = 0.0000028    # 2.8nS
-        s.gl = 0.0000028    # 2.8nS
+        s.gNap = 2.8    # 2.8nS
+        s.gl = 2.8    # 2.8nS
         s.Vl = -65.0   # -65mV 
         s.Iapp = 0.0    # Zero unless otherwise noted
-        s.C = 0.000000021    # 21pF
+        s.C = 21.0   # 21pF
    
     def rhs(self, V , h, n):
         s = self
-        thV = 10 / (np.cosh((V + 48.0) / (12.0)))    
-        tnV = 10000/ (np.cosh((V + 29.0) / (-8.0)))
+        tauhV = 0.010 / (np.cosh((V + 48.0) / (12.0)))     # Conversion from ms to s
+        taunV = 10/ (np.cosh((V + 29.0) / (-8.0))) # Conversion from ms to s
         hinfV = expit((V + 48.0)/ (6.0))
         minfNaPV = spec.expit((V + 40.0)/ (-6.0))
         minfNaV = spec.expit((V + 34.0)/ (-5.0))
@@ -38,15 +38,15 @@ class Rhs(object):
         # m n h need expit functions
         # V h n are the function parameters
         # dVdt = [- Inaph - INa - IK - IL - Itonice==0 + Iapp==0 ] / C
-        Iapp = 0.000000
+        Iapp = 182.000000
         dVdt = (
                 (-s.gNap * minfNaPV * h * (V - s.VNa))
                 + ((-s.gNa * (minfNaV**3) * (1 - n)) * (V - s.VNa))
                 + ((-s.gk * (ninfNaV**4)) * (V - s.Vk))
                 + (-s.gl * (V - s.Vl))
-                + Iapp) / s.C
-        dhdt = (hinfV - h) / thV
-        dndt = (ninfNaV - n) / tnV
+                + Iapp) / s.C *1000 #Conversion to mV because C is defined in terms of Farad Volts
+        dhdt = (hinfV - h) / tauhV 
+        dndt = (ninfNaV - n) / taunV
         return dVdt, dhdt, dndt
     
     def __call__(self, X, t):
@@ -81,7 +81,7 @@ r.A = A
 nominal = r.gNa
 r.gNa = np.random.uniform(low = nominal*0.9, high = nominal*1.1, size = (N,))
         
-Xvals, Tvals = r.integrate(0, 25000)
+Xvals, Tvals = r.integrate(0, 1)
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 fig = plt.figure()
@@ -95,7 +95,7 @@ ax.set_ylabel('degree')
 ax.set_zlabel('V [mV]')
 
 fig, ax = plt.subplots()
-ax.plot(Tvals, Xvals)
+ax.plot(Tvals, X[: , 0, :])
 ax.set_xlabel('Time')
 ax.set_ylabel('MilliVolts')
 plt.show()
